3d2b11c1a490c40d0566a13e28dc62dfa67fd6ed
Tim Wilson <twilsonco@gmail.com>
2020-05-30 12:31:08 -0600

    bug fixes and improvements
    now handles single row/column, improved handling of user arguments, preserves user options between repeated runs, and will return to chronological order of images if 'shuffle' is disabled on a subsequent run.

--- PhotoCollagePythonista.py
+++ PhotoCollagePythonista.py
@@ -1,7 +1,7 @@
 '''
 Created on May 24, 2020
 
-@author: Tim Wilson
+@author: Tim Wilson twilsonco @t gmail d.t com
 '''
 import dialogs
 import photos
@@ -11,9 +11,18 @@ import datetime
 import random
 import math
 import os
-from PIL.ExifTags import TAGS
 from PIL import ImageOps
 from PIL import Image
+
+defaultArgs = {
+    'imagegap':'0',
+    'width':'5000',
+    'height':'5000',
+    'initheight':'500',
+    'aspectratiofactor':'1.0',
+    'shuffle':'True',
+    'noantialias':'True'
+}
 
 # start partition problem algorithm from https://stackoverflow.com/a/7942946
 # modified to act on list of images rather than the weights themselves
@@ -77,21 +86,25 @@ def makeCollage(imgList, spacing = 0, antialias = False, background=(0,0,0), asp
     avgWidth = totalWidth / len(imgList)
     targetWidth = avgWidth * math.sqrt(len(imgList) * aspectratiofactor)
     
-    numRows = int(round(totalWidth / targetWidth))
-    numRows = 2 if numRows < 2 else numRows
-    aspectRatios = [int(img.width / img.height * 100) for img in imgList]
-    
-    # get nested list of images (each sublist is a row in the collage)
-    imgRows = linear_partition(aspectRatios, numRows, imgList)
-    
-    # scale down larger rows to match the minimum row width
-    rowWidths = [sum([img.width + spacing for img in row]) - spacing for row in imgRows]
-    minRowWidth = min(rowWidths)
-    rowWidthRatios = [minRowWidth / w for w in rowWidths]
-    if antialias:
-        imgRows = [[img.resize((int(img.width * widthRatio), int(img.height * widthRatio)), Image.ANTIALIAS) for img in row] for row,widthRatio in zip(imgRows, rowWidthRatios)]
+    numRows = clamp(int(round(totalWidth / targetWidth)), 1, len(imgList))
+    if numRows == 1:
+        imgRows = [imgList]
+    elif numRows == len(imgList):
+        imgRows = [[img] for img in imgList]
     else:
-        imgRows = [[img.resize((int(img.width * widthRatio), int(img.height * widthRatio))) for img in row] for row,widthRatio in zip(imgRows, rowWidthRatios)]
+        aspectRatios = [int(img.width / img.height * 100) for img in imgList]
+    
+        # get nested list of images (each sublist is a row in the collage)
+        imgRows = linear_partition(aspectRatios, numRows, imgList)
+    
+        # scale down larger rows to match the minimum row width
+        rowWidths = [sum([img.width + spacing for img in row]) - spacing for row in imgRows]
+        minRowWidth = min(rowWidths)
+        rowWidthRatios = [minRowWidth / w for w in rowWidths]
+        if antialias:
+            imgRows = [[img.resize((int(img.width * widthRatio), int(img.height * widthRatio)), Image.ANTIALIAS) for img in row] for row,widthRatio in zip(imgRows, rowWidthRatios)]
+        else:
+            imgRows = [[img.resize((int(img.width * widthRatio), int(img.height * widthRatio))) for img in row] for row,widthRatio in zip(imgRows, rowWidthRatios)]
     
     # pupulate new image
     rowWidths = [sum([img.width + spacing for img in row]) - spacing for row in imgRows]
@@ -116,83 +129,108 @@ def makeCollage(imgList, spacing = 0, antialias = False, background=(0,0,0), asp
         continue
     
     return outImg
+    
+def getArgs(args = None):
+    import copy
+    
+    if args is None:
+        args = copy.deepcopy(defaultArgs)
+    else:
+        args['imagegap'] = str(clamp(int(args['imagegap']), 0, 1000) if 'imagegap' in args else 0)
+        args['width'] = str(max([int(args['width']), 0]) if 'width' in args else 5000)
+        args['height'] = str(max([int(args['height']), 0]) if 'height' in args else 5000)
+        args['initheight'] = str(max([int(args['initheight']), 0]) if 'initheight' in args else 500)
+        args['aspectratiofactor'] = str(float(clamp(float(args['aspectratiofactor']), 0.05, 20.0)) if 'aspectratiofactor' in args else 1.0)
+        args['shuffle'] = str(True if args['shuffle'] == 'True' else False)
+        args['noantialias'] = str(True if args['noantialias'] == 'True' else False)
+        
+    args = dialogs.form_dialog(title='Photo Collage', sections=[
+                ('Options', [
+                    {'title':'Shuffle images:   ',
+                     'key':'shuffle',
+                     'type':'check',
+                     'value':args['shuffle']},
+                    {'title':'Spacing*:   ',
+                     'key':'imagegap',
+                     'type':'number',
+                     'value':args['imagegap']},
+                    {'title':'Collage width*:   ',
+                     'key':'width',
+                     'type':'number',
+                     'value':args['width']},
+                    {'title':'Collage height**:   ',
+                     'key':'height',
+                     'type':'number',
+                     'value':args['height']}
+                    ],
+                    '*in pixels; **not used if width > 0'
+                ),
+                ('Advanced', [
+                    {'title':'Initial image height*:   ',
+                     'key':'initheight',
+                     'type':'number',
+                     'value':args['initheight']},
+                    {'title':'Scale collage aspect ratio**:   ',
+                     'key':'aspectratiofactor',
+                     'type':'number',
+                     'value':args['aspectratiofactor']},
+    #                 {'title':'Background color***:   ',
+    #                  'key':'background',
+    #                  'type':'text',
+    #                  'value':'0,0,0'},
+                    {'title':'Disable intermediate antialiasing***:   ',
+                     'key':'noantialias',
+                     'type':'check',
+                     'value':args['noantialias']}],
+                    '*lower values run faster and use less memory; **scaling mean image AR; ***final resize is always antialiased'
+                )])
+                
+    try:
+        args['imagegap'] = clamp(int(args['imagegap']), 0, 1000) if 'imagegap' in args else 0
+        args['width'] = max([int(args['width']), 0]) if 'width' in args else 5000
+        args['height'] = max([int(args['height']), 0]) if 'height' in args else 5000
+        args['initheight'] = max([int(args['initheight']), 0]) if 'initheight' in args else 500
+        args['aspectratiofactor'] = float(clamp(float(args['aspectratiofactor']), 0.05, 20.0)) if 'aspectratiofactor' in args else 1.0
+        args['shuffle'] = True if args['shuffle'] == 'True' or args['shuffle'] else False
+        args['noantialias'] = True if args['noantialias'] == 'True' or args['noantialias'] else False
+        userExit = 2
+    except:
+        userExit = dialogs.alert(title='Photo Collage', message='There was one or more input errors. Check that your input values make sense and try again', button1='OK',button3='Quit',hide_cancel_button=True)
+    
+    return args,userExit
 
 # modified (significantly) from https://github.com/delimitry/collage_maker
 # this main function is for the CLI implementation
 def main():
     console.clear()
     userExit = 0
+    args = None
+    imageAssets = []
+    pilImages = []
     
     while userExit <= 1:
         # get images
-        pilImages = []
-        images = photos.pick_asset(title='Select images for collage', assets=photos.get_assets(), multi=True)
-        if images is None:
+        if len(imageAssets) < 2:
+            pilImages = []
+            imageAssets = photos.pick_asset(title='Select images for collage', assets=photos.get_assets(), multi=True)
+        if imageAssets is None:
             return
         
         # only keep photos
-        images = [img for img in images if img.media_type == 'image']
+        imageAssets = [img for img in imageAssets if img.media_type == 'image']
         
-        if len(images) < 3:
+        if len(imageAssets) < 2:
             userExit = dialogs.alert(title="Photo Collage", message="Select 3 or more images.", button1='OK', button3="Quit", hide_cancel_button=True)
         
-        while userExit <= 2 and len(images) >= 3:
+        while userExit <= 2 and len(imageAssets) >= 2:
             # get user options
-        #     while True:
-            args = dialogs.form_dialog(title='Photo Collage', sections=[
-                    ('Options', [
-                        {'title':'Shuffle images:   ',
-                         'key':'shuffle',
-                         'type':'check',
-                         'value':'True'},
-                        {'title':'Spacing*:   ',
-                         'key':'imagegap',
-                         'type':'number',
-                         'value':'0'},
-                        {'title':'Collage width*:   ',
-                         'key':'width',
-                         'type':'number',
-                         'value':'5000'},
-                        {'title':'Collage height**:   ',
-                         'key':'height',
-                         'type':'number',
-                         'value':'5000'}
-                        ],
-                        '*in pixels; **not used if width > 0'
-                    ),
-                    ('Advanced', [
-                        {'title':'Initial image height*:   ',
-                         'key':'initheight',
-                         'type':'number',
-                         'value':'500'},
-                        {'title':'Scale collage aspect ratio**:   ',
-                         'key':'aspectratiofactor',
-                         'type':'number',
-                         'value':'1.0'},
-                        {'title':'Disable intermediate antialiasing***:   ',
-                         'key':'noantialias',
-                         'type':'check',
-                         'value':'True'}],
-                        '*lower values run faster and use less memory; **scaling mean image AR; ***final resize is always antialiased'
-                    )])
-                            
-            try:
-                args['imagegap'] = clamp(int(args['imagegap']), 0, 1000) if 'imagegap' in args else 0
-                args['width'] = max([int(args['width']), 0]) if 'width' in args else 5000
-                args['height'] = max([int(args['height']), 0]) if 'height' in args else 5000
-                args['initheight'] = max([int(args['initheight']), 0]) if 'initheight' in args else 500
-                args['aspectratiofactor'] = float(clamp(float(args['aspectratiofactor']), 0.05, 20.0)) if 'aspectratiofactor' in args else 1.0
-                args['shuffle'] = True if args['shuffle'] == 'True' else False
-                args['noantialias'] = True if args['noantialias'] == 'True' else False
-            except:
-                userExit = dialogs.alert(title='Photo Collage', message='There was one or more input errors. Check that your input values make sense and try again', button1='OK',button3='Quit',hide_cancel_button=True)
-                continue
+            args,userExit = getArgs(args)
                 
             # get PIL image objects for all the photos
-            if userExit <= 1 or len(pilImages) != len(images):
+            if userExit <= 1 or len(pilImages) != len(imageAssets):
                 print('Loading photos...')
                 pilImages = []
-                for f in images:
+                for f in imageAssets:
                     img = f.get_image()
                     if args['initheight'] >= 50 and img.height > args['initheight']:
                         if args['noantialias']:
@@ -201,14 +239,18 @@ def main():
                             pilImages.append(img.resize((int(img.width / img.height * args['initheight']), args['initheight']), Image.ANTIALIAS))
                     else:
                         pilImages.append(img)
+                imageList = [(i,imageAssets[i],pilImages[i]) for i in range(len(pilImages))]
+                
             
             # shuffle images if needed
             if args['shuffle']:
-                random.shuffle(pilImages)
+                random.shuffle(imageList)
+            else:
+                imageList.sort(key=lambda img:img[0])
                 
             print('Making collage...')
             
-            collage = makeCollage(pilImages, spacing=args['imagegap'], antialias=not args['noantialias'], aspectratiofactor=args['aspectratiofactor'])
+            collage = makeCollage([i[2] for i in imageList], spacing=args['imagegap'], antialias=not args['noantialias'], aspectratiofactor=args['aspectratiofactor'])
             
             if args['width'] >= 50 and collage.width > args['width']:
                 collage = collage.resize((args['width'], int(collage.height / collage.width * args['width'])), Image.ANTIALIAS)
@@ -229,9 +271,12 @@ def main():
             os.remove(path)
             
             userExit = dialogs.alert(title="Finished!", button1='New w/ new pics', button2='New w/ same pics', button3='Quit', hide_cancel_button=True)
+            if userExit == 1:
+                imageAssets = []
             
             print('Finished!')
 
 
 if __name__ == '__main__':
     main()
+    pass
